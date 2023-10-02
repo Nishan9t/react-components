fd92f039324b502d62cd7bf5ec837f1590a6a5b9
Nishan9t <pandeynisha9t@gmail.com>
2023-10-02 11:43:56 +0530

    sidebar is created

--- src/components/SlideInComponent.jsx
+++ src/components/SlideInComponent.jsx
@@ -3,36 +3,24 @@ import React, { useState } from "react";
 
 const SlideInComponent = () => {
 
-    
-  const [isVisible, setIsVisible] = useState(false);
-
-  const toggleVisibility = () => {
-    setIsVisible(!isVisible);
-  };
-
+  
   return (
-    <div className="flex flex-col items-center mt-8">
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={toggleVisibility}
-      >
-        Toggle Slide In
-      </button>
-
-      {isVisible && (
-        <div className="fixed inset-0 flex left-0 z-50 ">
-          <div className=" relative left-0 bg-white p-8 rounded shadow-lg transform translate-x-full transition-transform duration-700">
-            <h2 className="text-2xl font-bold mb-4">Slide In Content</h2>
-            <p>This content slides in from the right.</p>
-            <button
-              className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-              onClick={toggleVisibility}
-            >
-              Close
-            </button>
+    <div className="w-full">
+        <div className="w-1/5 min-h-screen bg-gray-400 p-4 text-center items-center">
+          <h1 className="font-bold text-xl underline text-white">Nishant Pandey</h1>
+          <div className="mt-4 text-white ">
+          <ul className="text-left ">
+            <li className="my-2 hover:text-red-500 cursor-pointer">Dashboard</li>
+            <hr/>
+            <li className="my-2 hover:text-red-500 cursor-pointer">Products</li>
+            <hr/>
+            <li className="my-2 hover:text-red-500 cursor-pointer">Contact</li>
+            <hr/>
+            <li className="my-2 hover:text-red-500 cursor-pointer">About</li>
+            <hr/>
+          </ul>
           </div>
-        </div>
-      )}
+        </div>     
     </div>
   );
 };
