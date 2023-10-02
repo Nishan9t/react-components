c093bcb10040b5ee5016fd70178367198a95f879
Nishan9t <pandeynisha9t@gmail.com>
2023-10-02 13:21:38 +0530

    now sidebar is sliding

--- src/components/SlideInComponent.jsx
+++ src/components/SlideInComponent.jsx
@@ -3,17 +3,29 @@ import React, { useState } from "react";
 
 const SlideInComponent = () => {
 
-  const [active,setActive]=useState(false)
+   const [active,setActive]=useState("<")
+   const [open,setOpen]=useState(true)
   
   return (
     <div className="flex">
         
-        <div className={`w-72 h-screen bg-gray-800 relative `}>
-        <button className=" absolute cursor-pointer -right-2 top-9 border-2 rounded-xl px-2 text-white font-bold bg-red-500">{`>`}</button>
-          
+        <div className={`${open ? "w-1/4" :"w-10"} duration-500 h-screen bg-gray-800 relative md:text-center `}>
+        <button className={`absolute cursor-pointer -right-0 top-0 border-2 rounded-full px-2 text-white font-bold bg-red-500 ${!open && 'rotate-180'}` }
+        onClick={()=>{
+          setOpen(!open)
+          // if(active ===">")
+          // {
+          //   setActive("<")
+          // }
+          // else{
+          //   setActive(">")
+          // }
+        }}  
+        >{active}</button>
+          <span className={`${!open && 'hidden'} origin-left duration-500`}>
           <h1 className="font-bold text-xl underline text-white">Nishant Pandey</h1>
           <div className="mt-4 text-white ">
-            <ul className="text-left ">
+            <ul className="text-left md:mx-4 ">
               <li className="my-2 hover:text-red-500 cursor-pointer">Dashboard</li>
               <hr/>
               <li className="my-2 hover:text-red-500 cursor-pointer">Products</li>
@@ -23,7 +35,9 @@ const SlideInComponent = () => {
               <li className="my-2 hover:text-red-500 cursor-pointer">About</li>
               <hr/>
             </ul>
+           
           </div>
+          </span>
           
           
         </div> 
