8a5f664c16276e4d9a4aa1b98b70e84769d9c582
Nishan9t <pandeynisha9t@gmail.com>
2023-10-04 11:59:08 +0530

    framer inital motion practice

--- src/components/Framermotion.jsx
+++ src/components/Framermotion.jsx
@@ -3,8 +3,14 @@ import { motion } from 'framer-motion'
 
 export default function Framermotion() {
   return (
-    <div className='w-screen h-screen'>
-    <motion.h1 className='text-center text-2xl ' animate={
+    <motion.div className='my-auto items-center'
+    animate={{
+        marginTop:16,
+        opacity:0.8,
+        rotateZ: -20
+    }}>
+    <motion.h1 className='text-center text-2xl '
+     animate={
         {
             fontSize:200,
             color:'#ff2994',
@@ -14,10 +20,14 @@ export default function Framermotion() {
     }>
         Welcome to Pizza Joint
     </motion.h1>
-    <button>
+    <motion.button className=' border-2  p-2 ml-16'
+    animate={{
+        scale:1.5,
+        color:'#ff2342'
+    }} >
         Create Your Pizza
-    </button>
+    </motion.button>
 
-    </div>
+    </motion.div>
   )
 }
