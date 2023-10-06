670996e80f386fb53843528bb48f443e0dd69d6d
Nishan9t <pandeynisha9t@gmail.com>
2023-10-06 10:54:55 +0530

    dots is also implemented

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import {BsChevronLeft,BsChevronRight} from 'react-icons/bs'
+import {RxDotFilled} from 'react-icons/rx'
+
 
 export default function Slider() {
 
@@ -36,6 +38,11 @@ export default function Slider() {
 
     }
 
+    const goToSlide=(slideIndex)=>{
+        setCurrentIndex(slideIndex)
+
+    }
+
   return (
     //providing class of group
     <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
@@ -49,6 +56,17 @@ export default function Slider() {
         {/* right arrow */}
         <div className='hidden group-hover:block absolute top-[50%] -translate-x-0  translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
             <BsChevronRight size={30} onClick={nextSlide}/>
+        </div>
+
+        {/* dots */}
+        <div className='flex top-4 justify-center py-2'>
+        {
+            slides.map((slide,slideIndex)=>(
+                <div key={slideIndex} onClick={()=>goToSlide(slideIndex)} className='text-2xl cursor-pointer'>
+                    <RxDotFilled className={`${currentIndex===slideIndex&&'text-red-500'}`}/>
+                </div>
+            ))
+        }
         </div>
 
     </div>
