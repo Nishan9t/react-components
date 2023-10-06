f9935498ea70851769199f839adbc38a94676741
Nishan9t <pandeynisha9t@gmail.com>
2023-10-06 09:41:55 +0530

    button is added with group hover effect

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {BsChevronLeft,BsChevronRight} from 'react-icons/bs'
 
 export default function Slider() {
 
@@ -21,6 +22,19 @@ export default function Slider() {
     ]
 
   return (
-    <div>Slider</div>
+    <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
+        <div style={{backgroundImage:`url(${slides[0].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'> </div>
+
+        {/* left arrow */}
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0  translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+            <BsChevronLeft size={30}/>
+        </div>
+
+        {/* right arrow */}
+        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0  translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+            <BsChevronRight size={30}/>
+        </div>
+
+    </div>
   )
 }
