1c8bed56c5937babb87214ff1844ea0ddf8cbf1e
Nishan9t <pandeynisha9t@gmail.com>
2023-10-06 10:41:57 +0530

    images are now changing with click

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BsChevronLeft,BsChevronRight} from 'react-icons/bs'
 
 export default function Slider() {
@@ -19,20 +19,36 @@ export default function Slider() {
         {
             url:'https://images.unsplash.com/photo-1468436139062-f60a71c5c892?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
         },
-    ]
+    ];
+
+    const [currentIndex,setCurrentIndex]=useState(0)
+
+    const prevSlide=()=>{
+        const ifFirstSlide= currentIndex===0;
+        const newIndex = ifFirstSlide ? slides.length-1 : currentIndex-1;
+        setCurrentIndex(newIndex);
+
+    }
+    const nextSlide=()=>{
+        const ifLastSlide = currentIndex===slides.length-1;
+        const newIndex = ifLastSlide ? 0 : currentIndex+1;
+        setCurrentIndex(newIndex);
+
+    }
 
   return (
+    //providing class of group
     <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
-        <div style={{backgroundImage:`url(${slides[0].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'> </div>
+        <div style={{backgroundImage:`url(${slides[currentIndex].url})`}} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'> </div>
 
         {/* left arrow */}
         <div className='hidden group-hover:block absolute top-[50%] -translate-x-0  translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-            <BsChevronLeft size={30}/>
+            <BsChevronLeft size={30} onClick={prevSlide}/>
         </div>
 
         {/* right arrow */}
         <div className='hidden group-hover:block absolute top-[50%] -translate-x-0  translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-            <BsChevronRight size={30}/>
+            <BsChevronRight size={30} onClick={nextSlide}/>
         </div>
 
     </div>
