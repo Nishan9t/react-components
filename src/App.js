594a17e0356c75a35cf49790743f7afb77d842da
Nishan9t <pandeynisha9t@gmail.com>
2023-10-06 09:26:25 +0530

    slider images is formed in array of objects

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 
-import './App.css';
+// import './App.css';
 import DropNavbar from './components/DropNavbar';
 import Framermotion from './components/Framermotion';
 
 import SlideInComponent from './components/SlideInComponent';
+import Slider from './components/Slider';
 
 function App() {
   return (
     <div>
     {/* <SlideInComponent/> */}
     {/* <DropNavbar/> */}
-    <Framermotion/>
+    {/* <Framermotion/> */}
+    <Slider/>
 
     </div>
   );
---
+++ src/components/Slider.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Slider() {
+
+    const slides=[
+        {
+            url:'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2020&q=80'
+        },
+        {
+            url:'https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
+        },
+        {
+            url:'https://images.unsplash.com/photo-1611162617213-7d7a39e9b1d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80'
+        },
+        {
+            url:'https://plus.unsplash.com/premium_photo-1685283298967-88bdc86e9c40?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1925&q=80'
+        },
+        {
+            url:'https://images.unsplash.com/photo-1468436139062-f60a71c5c892?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
+        },
+    ]
+
+  return (
+    <div>Slider</div>
+  )
+}
