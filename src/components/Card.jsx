216d2e79a25a6786a8f74a04696b13fa45d0401b
Nishan9t <pandeynisha9t@gmail.com>
2023-10-12 11:30:30 +0530

    cards is completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 export default function Card() {
   return (
-    <div className=' flex w-full mt-16 p-4 bg-green-400'>
-        <div className='w-1/3 bg-red-200 m-2 py-2 px-4'>
-                <h1>Card 1</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+    <div className=' flex w-full h-96 mt-16 p-4'>
+        <div className='w-1/3 bg-red-200 m-2 py-2 px-4 text-center rounded-md hover:scale-75'>
+                <h1 className='text-2xl font-bold '>Card 1</h1>
+                <p className='py-2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
         </div>
-        <div className='w-1/3 bg-red-200  m-2 py-2 px-4'>
-                <h1>Card 1</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+        <div className='w-1/3 bg-red-200  m-2 py-2 px-4 text-center rounded-md hover:scale-75'>
+                <h1 className='text-2xl font-bold '>Card 1</h1>
+                <p className='py-2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
         </div>
-        <div className='w-1/3 bg-red-200  m-2 py-2 px-4'>
-                <h1>Card 1</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+        <div className='w-1/3 bg-red-200  m-2 py-2 px-4 text-center rounded-md hover:scale-75'>
+                <h1 className='text-2xl font-bold '>Card 1</h1>
+                <p className='py-2'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
         </div>
     </div>
   )
