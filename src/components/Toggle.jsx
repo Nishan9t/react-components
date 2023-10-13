c08d30d0b513a4c41753f6e498858f640e1cf4cc
Nishan9t <pandeynisha9t@gmail.com>
2023-10-13 19:42:25 +0530

    css is changed and toggle is working

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function Toggle() {
+
+    const [click,setClick]=useState(false);
+
   return (
     <div className='p-4'>
         <div className=' w-screen h-screen bg-green-200 items-center '>
-            <button className='border border-2 bg-white rounded-full p-1 w-96 h-32 relative'>
-                <div className='absolute rounded-full bg-gray-700 w-1/2 h-full top-0 right-0 '>
+            <button onClick={()=>setClick(!click)} className={`flex text-center items-center border border-2 bg-white rounded-full  w-[160px] h-[100px] ${click?'justify-end':'justify-start'}   duration-300`}>
+                <div className={` rounded-full bg-gray-700 w-[80px] h-[80px]  `} >
 
                 </div>
             </button>
