850e5ff316a8972682c329af5cf3fcf2689d7e37
Nishan9t <pandeynisha9t@gmail.com>
2023-10-13 19:51:01 +0530

    final commit of toggle

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -6,10 +6,10 @@ export default function Toggle() {
     const [click,setClick]=useState(false);
 
   return (
-    <div className='p-4'>
-        <div className=' w-screen h-screen bg-green-200 items-center '>
+    <div className='flex justify-center items-center'>
+        <div className={`w-screen h-screen items-center ${click?'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500':'bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%'}`}>
             <button onClick={()=>setClick(!click)} className={`flex text-center items-center border border-2 bg-white rounded-full  w-[160px] h-[100px] ${click?'justify-end':'justify-start'}   duration-300`}>
-                <div className={` rounded-full bg-gray-700 w-[80px] h-[80px]  `} >
+                <div className={` rounded-full bg-gray-700 w-[80px] h-[80px] ${click?'bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%':'bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'} `} >
 
                 </div>
             </button>
