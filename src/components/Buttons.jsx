ffd2c62b09d54b74fc51c08736c4523f51520f54
Nishan9t <pandeynisha9t@gmail.com>
2023-10-19 11:20:53 +0530

    animate effect is added on a button

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -7,8 +7,8 @@ export default function Buttons() {
             <button className='border-2 px-4 py-2 rounded-md bg-blue-500 hover:scale-x-95 duration-700'>Click</button>
             <button className='border-2 px-4 py-2 rounded-md bg-blue-500 hover:bg-slate-500 origin-center duration-1000'>Click</button>
             <button className='border-2 px-4 py-2 rounded-md bg-blue-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-500'>Click</button>
-            <button className='border-2 px-4 py-2 rounded-md bg-blue-500'>Click</button>
-            <button className='border-2 px-4 py-2 rounded-md bg-blue-500'>Click</button>
+            <button className='border-2 px-4 py-2 rounded-md bg-blue-500 hover:animate-bounce'>Click</button>
+            <button className='border-2 px-4 py-2 rounded-md hover:bg-green-700 hover-top duration-1000'>Click</button>
             <button className='border-2 px-4 py-2 rounded-md bg-blue-500'>Click</button>
             <button className='border-2 px-4 py-2 rounded-md bg-blue-500'>Click</button>
             <button className='border-2 px-4 py-2 rounded-md bg-blue-500'>Click</button>
