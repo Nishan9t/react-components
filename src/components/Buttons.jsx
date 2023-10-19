572398bf6a42b5090aebaf4426537fe80277b1e1
Nishan9t <pandeynisha9t@gmail.com>
2023-10-19 11:08:10 +0530

    hover effects are added on 3 buttons

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -4,9 +4,9 @@ export default function Buttons() {
   return (
     <div>
         <div className='flex flex-col w-1/5 mx-auto mt-16 gap-5'>
-            <button className='border-2 px-4 py-2 rounded-md bg-blue-500'>Click</button>
-            <button className='border-2 px-4 py-2 rounded-md bg-blue-500'>Click</button>
-            <button className='border-2 px-4 py-2 rounded-md bg-blue-500'>Click</button>
+            <button className='border-2 px-4 py-2 rounded-md bg-blue-500 hover:scale-x-95 duration-700'>Click</button>
+            <button className='border-2 px-4 py-2 rounded-md bg-blue-500 hover:bg-slate-500 origin-center duration-1000'>Click</button>
+            <button className='border-2 px-4 py-2 rounded-md bg-blue-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-500'>Click</button>
             <button className='border-2 px-4 py-2 rounded-md bg-blue-500'>Click</button>
             <button className='border-2 px-4 py-2 rounded-md bg-blue-500'>Click</button>
             <button className='border-2 px-4 py-2 rounded-md bg-blue-500'>Click</button>
